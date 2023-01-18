a8fd8c3a0295816d0544b9c06c23a0988cf9b610
jngreco <jnoegreco@gmail.com>
2023-01-17 18:31:50 -0600

    final edits

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -32,27 +32,28 @@ with open(election_data_csv) as csvfile:
         final_results[candidate_name] = final_results[candidate_name] + 1
     #print(f"{final_results}") #to check work: Charles Casper Stockham': 85213, 'Diana DeGette': 272892, 'Raymon Anthony Doane': 11606
 
-  #for loop for election winner and percentages 
-    for candidate in final_results:
-      votes = final_results.get(candidate)
-      vote_percent = (votes/total_votes) * 100
-      vote_percent = round(vote_percent,3)
-      #print(candidate) to check 
-      #print(vote_percent) to check work
-      #print(final_results[candidate_name]) #to check 
-      
-winner = max(final_results, key=final_results.get) # key function learned with tutor Asiha Braxton-Garvin
-  #print(winner) to check 
-
 with open(output_text, "w") as csvfile:
-  election_results_final = (
-    f"Election Results \n" 
-    f"---------------- \n"
-    f"Total Votes: {total_votes} \n"
-    f"{candidate}: %{vote_percent} ({final_results[candidate_name]}) \n"
-    f"Winner: {winner} \n")
+  title= (f"Election Results \n" #used to print information that should not be overridden in the loop
+          f"---------------- \n"
+          f"Total Votes: {total_votes} \n")
+  print(title)
+  csvfile.write(title)
 
-  print(election_results_final)
-
-  csvfile.write(election_results_final)
-
+  for candidate in final_results: #for loop for election winner and percentages 
+        votes = final_results.get(candidate)
+        vote_percent = (votes/total_votes) * 100
+        vote_percent = round(vote_percent,3)
+        #print(candidate) to check 
+        #print(vote_percent) to check work
+        #print(final_results[candidate_name]) #to check 
+        
+        election_results_final = (
+          f"{candidate}: %{vote_percent} ({final_results[candidate_name]})\n" #needed loop to print all candidate info
+        )
+        print(election_results_final)
+        csvfile.write(election_results_final)
+      
+        winner = max(final_results, key=final_results.get) # key function learned with tutor Asiha Braxton-Garvin
+  #print(winner) to check 
+  print(f"Winner: {winner} \n")
+  csvfile.write(f"Winner: {winner} \n")
